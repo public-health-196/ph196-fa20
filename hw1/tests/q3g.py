368dd376eda43ea0b7dd88d952b10620cd844bba
Sam Robertson <samantha_robertson@berkeley.edu>
2020-09-22 12:36:49 -0700

    fix up problem 1 and 3g test

--- hw1/tests/q3g.py
+++ hw1/tests/q3g.py
@@ -7,7 +7,6 @@ test = {
         {
           'code': r"""
           >>> assert type(ziadopril_effect) in (int, np.int64, float, np.float64), "The treatment effect should be a numeric type (integer or float)"
-          >>> assert ziadopril_effect < np.mean(with_zp_sysbp) - np.mean(without_zp["SYSBP"])
           >>> assert abs(ziadopril_effect - (-20)) <= 3
           """,
           'hidden': False,
